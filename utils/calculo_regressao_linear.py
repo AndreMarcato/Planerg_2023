a2b47cf4a6543be3dde52113677d628134be4df7
Lucca <lucca.viccini@engenharia.ufjf.br>
2023-08-18 11:35:19 -0300

    refact: add funcao recorte em utils

--- utils/calculo_regressao_linear.py
+++ utils/calculo_regressao_linear.py
@@ -1,5 +1,6 @@
 import numpy as np
 from cvxopt import matrix, solvers
+from utils.recorte import recorte
 
 def calculo_regressao_linear(Usina, mes, num_phis ,total_anos, num_anos, imprime):
 
@@ -18,41 +19,13 @@ def calculo_regressao_linear(Usina, mes, num_phis ,total_anos, num_anos, imprime
 
   # print("P:", np.array(P))
   P = matrix(P)
-  
-  
 
-
-
-  def recorte(mes, nome):
-    
-
-    if (nome == 'Aeq'):
-      if mes - num_phis >= 0:
-        recorte =  Usina['vazoes'][1:total_anos, mes-num_phis:mes]
-        recorte = recorte[:, ::-1]                               # Para espelhar a matriz e ficar com os dados de trás para frente
-        return recorte
-      else:
-        recorte_ano_atual    =  Usina['vazoes'][1:total_anos, 0:mes]
-        recorte_ano_atual = recorte_ano_atual[:, ::-1]           # Para espelhar a matriz e ficar com os dados de trás para frente
-
-        recorte_ano_anterior =  Usina['vazoes'][0:total_anos-1, (mes-num_phis):]
-        recorte_ano_anterior = recorte_ano_anterior[:, ::-1]     # Para espelhar a matriz e ficar com os dados de trás para frente
-
-        recorte = matrix(np.concatenate((recorte_ano_atual, recorte_ano_anterior), axis=1))
-        return recorte
-
-    if (nome == 'Beq'):
-      recorte = Usina['vazoes'][1:total_anos, mes:mes+1]
-      return recorte
-
-
-
-  recorte_Aeq = recorte(mes, 'Aeq')
+  recorte_Aeq = recorte(mes, 'Aeq', Usina, num_phis, total_anos)
   matriz_id_1 = np.eye(num_anos)
 
   Aeq = matrix(np.concatenate((recorte_Aeq, matriz_id_1), axis=1))
 
-  Beq = matrix(recorte(mes, 'Beq'))
+  Beq = matrix(recorte(mes, 'Beq', Usina, num_phis, total_anos))
   Beq = Beq*1.0
 
 
--- utils/recorte.py
+++ utils/recorte.py
@@ -2,29 +2,21 @@ import numpy as np
 from cvxopt import matrix
 
 
-def recorte(Usina, mes, nome, num_phis, total_anos):
-  num_mes = mes - 1
-
+def recorte(mes, nome, Usina, num_phis, total_anos):
+  
   if (nome == 'Aeq'):
-    if num_mes - num_phis >= 0:
-      recorte = Usina['vazoes'][1:total_anos, num_mes-num_phis:num_mes]
-      # Para espelhar a matriz e ficar com os dados de trás para frente
-      recorte = recorte[:, ::-1]
+    if mes - num_phis >= 0:
+      recorte =  Usina['vazoes'][1:total_anos, mes-num_phis:mes]
+      recorte = recorte[:, ::-1]                               # Para espelhar a matriz e ficar com os dados de trás para frente
       return recorte
     else:
-      recorte_ano_atual = Usina['vazoes'][1:total_anos, 0:num_mes]
-      # Para espelhar a matriz e ficar com os dados de trás para frente
-      recorte_ano_atual = recorte_ano_atual[:, ::-1]
-
-      recorte_ano_anterior = Usina['vazoes'][0:total_anos -
-                                             1, (num_mes-num_phis):]
-      # Para espelhar a matriz e ficar com os dados de trás para frente
-      recorte_ano_anterior = recorte_ano_anterior[:, ::-1]
-
-      recorte = matrix(np.concatenate(
-          (recorte_ano_atual, recorte_ano_anterior), axis=1))
+      recorte_ano_atual    =  Usina['vazoes'][1:total_anos, 0:mes]
+      recorte_ano_atual = recorte_ano_atual[:, ::-1]           # Para espelhar a matriz e ficar com os dados de trás para frente
+      recorte_ano_anterior =  Usina['vazoes'][0:total_anos-1, (mes-num_phis):]
+      recorte_ano_anterior = recorte_ano_anterior[:, ::-1]     # Para espelhar a matriz e ficar com os dados de trás para frente
+      recorte = matrix(np.concatenate((recorte_ano_atual, recorte_ano_anterior), axis=1))
       return recorte
-
   if (nome == 'Beq'):
-    recorte = Usina['vazoes'][1:total_anos, num_mes:num_mes+1]
+    recorte = Usina['vazoes'][1:total_anos, mes:mes+1]
     return recorte
+
